71aa539ae034f24dc0b1dd8e2a46d80780357c9b
Zahin Ahmed <ahmednisa22@gmail.com>
2022-02-18 19:42:01 +0600

    closed_open detection model added

---
+++ closed_open1.py
@@ -0,0 +1,148 @@
+import os
+import numpy as np
+import pandas as pd
+from PIL import Image
+import tensorflow as tf
+from keras.applications.resnet import ResNet50
+from keras_preprocessing.image import ImageDataGenerator
+from sklearn.metrics import classification_report, confusion_matrix
+
+np.random.seed(4)
+
+
+def prepare_df(data_type):
+    X = []
+    y = []
+    path1 = './dataset_new/' + data_type + '/co'
+    for i in os.listdir(path1):
+        # Image
+        X.append(i)
+        # Label
+        y.append(i.split('_')[0])
+    # path2 = './dataset_new/' + data_type + '/co/open/'
+    # for i in os.listdir(path2):
+    #     # Image
+    #     X.append(i)
+    #     # Label
+    #     y.append("1")
+
+    X = np.array(X)
+    y = np.array(y)
+    # print(X.shape)
+
+    # shuffle = np.random.permutation(X.shape[0])
+    # X = X[shuffle]
+    # y = y[shuffle]
+    # print(X[0:10])
+
+    df = pd.DataFrame()
+    df['filename'] = X
+    df['label'] = y
+    # print(df['filename'].head(20))
+    return df
+
+
+def get_datagen():
+
+    datagen = ImageDataGenerator(
+        # featurewise_center=True,
+        # featurewise_std_normalization=True,
+        # width_shift_range=0.2,
+        # height_shift_range=0.2,
+        rotation_range=20,
+        zoom_range=0.2,
+        horizontal_flip=True,
+    )
+    return datagen
+
+
+def get_data_flow(datagen, df, folder):
+    path = './dataset_new/' + folder + '/co/'
+    d_generator = datagen.flow_from_dataframe(
+        df,
+        directory=path,
+        x_col='filename',
+        y_col='label',
+        class_mode='binary',
+        target_size=(256, 256),
+    )
+    return d_generator
+
+
+def get_model():
+
+    feature_extractor = ResNet50(weights='imagenet',
+                                 input_shape=(256, 256, 3),
+                                 include_top=False)
+
+    feature_extractor.trainable = False
+
+    input_ = tf.keras.Input(shape=(256, 256, 3))
+
+    x = feature_extractor(input_, training=False)
+
+    x = tf.keras.layers.GlobalAveragePooling2D()(x)
+
+    output_ = tf.keras.layers.Dense(1, activation='sigmoid')(x)
+
+    model = tf.keras.Model(input_, output_)
+
+    model.compile(optimizer='adam',
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
+
+    model.summary()
+    return model
+
+
+def prediction(model):
+    y_true = []
+    y_pred = []
+
+    for i in os.listdir('./dataset_new/test/co/'):
+        img = Image.open('./dataset_new/test/co/' + i)
+        img = img.resize((256, 256))
+        img = np.array(img)
+        img = np.expand_dims(img, 0)
+
+        y_true.append(int(i.split('_')[0]))
+        y_pred.append(1 if model.predict(img) > 0.5 else 0)
+
+    print(classification_report(y_true, y_pred))
+    print()
+    print(confusion_matrix(y_true, y_pred))
+
+
+def main():
+    df_train = prepare_df("train")
+    df_test = prepare_df("test")
+    train_datagen = get_datagen()
+    test_datagen = get_datagen()
+    train_generator = get_data_flow(train_datagen, df_train, "train")
+    test_generator = get_data_flow(test_datagen, df_test, "test")
+
+    model = get_model()
+    # model.fit(train_generator, epochs=1, validation_data=test_generator)
+    # prediction(model)
+
+    # train_generator = train_datagen.flow_from_dataframe(
+    #     df_train,
+    #     directory='./dataset_new/train/co/',
+    #     x_col='filename',
+    #     y_col='label',
+    #     class_mode='binary',
+    #     target_size=(256, 256),
+    # )
+    #
+    # test_generator = test_datagen.flow_from_dataframe(
+    #     df_test,
+    #     directory='./dataset_new/test/co/',
+    #     x_col='filename',
+    #     y_col='label',
+    #     class_mode='binary',
+    #     target_size=(256, 256),
+    # )
+
+
+if __name__ == '__main__':
+    main()
